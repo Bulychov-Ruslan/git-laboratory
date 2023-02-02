fafef313349a208f1d45df8431e4d9bde708ce5f
Bulychov-Ruslan <rusbul02@gmail.com>
2023-02-02 17:10:31 +0600

    lab3

---
+++ lab3/main.py
@@ -0,0 +1,42 @@
+
+# a = int(input())
+# b = int(input())
+
+# for i in range(a, b + 1):
+#     print(i, end=' ')
+
+
+
+# a = int(input())
+# b = int(input())
+# if a < b:
+#     for i in range(a, b + 1):
+#         print(i, end=' ')
+# else:
+#     for i in range(a, b - 1, -1):
+#         print(i, end=' ')
+
+
+
+# a = int(input())
+# b = int(input())
+# while a>b:
+#     while a%2==1:
+#         print(a, end=' ')
+#         break
+#     a -= 1
+
+
+# import random
+
+# n = int(input('N = '))
+# x = random.randrange(1, n)
+# a=i=1
+# sum1=0
+# sum=0
+# for i in range(n):
+#     sum1+=i
+#     if i==x:
+#         continue
+#     sum+=i
+# print(sum1-sum)
